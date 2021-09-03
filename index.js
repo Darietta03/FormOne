2685b74ff5b3594227b0c6fdb1c411074c49963f
Darietta03 <dsnegireva2017@gmail.com>
2021-09-03 23:21:52 +0300

    firstchanges

---
+++ index.js
@@ -0,0 +1,10 @@
+const chooseCountry = document.querySelector('.label__country');
+const lastname = document.querySelector('.form-row__lastname');
+console.log(chooseCountry);
+
+
+
+if (chooseCountry.getAttribute('USA')) {
+   lastname.style.display = 'none';
+}
+
